777fcc4681e1f180574ceac6d942740f58835ae7
Richard Cardoso <richard.166@hotmail.com>
2024-01-22 23:59:00 -0300

    chore: cleanup

--- src/test/java/br/fiap/projeto/identificacao/ClienteServiceTest.java
+++ src/test/java/br/fiap/projeto/identificacao/ClienteServiceTest.java
@@ -1,86 +1,161 @@
 package br.fiap.projeto.identificacao;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-// @SpringBootTest
-// @TestPropertySource("classpath:application.properties")
-// public class ClienteServiceTest {
+import java.util.Optional;
 
-// @Autowired
-// private IClienteRestAdapterController clienteRestAdapterController;
+import br.fiap.projeto.identificacao.usecase.GestaoClienteUseCase;
+import br.fiap.projeto.identificacao.usecase.exception.EntidadeNaoEncontradaException;
+import br.fiap.projeto.identificacao.usecase.exception.EntradaInvalidaException;
+import br.fiap.projeto.identificacao.usecase.port.IClienteRepositoryAdapterGateway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// @Test
-// public void testeBusca() {
-// List<ClienteResponseDTO> clienteDTOS =
-// clienteRestAdapterController.buscaTodos();
-// assertFalse(CollectionUtils.isEmpty(clienteDTOS));
-// }
+import br.fiap.projeto.identificacao.entity.Cliente;
 
-// @Test
-// public void testeBuscaPorCpf() throws EntidadeNaoEncontradaException,
-// EntradaInvalidaException {
-// String cpf = "09876543210";
-// ClienteRequestDTO request = new ClienteRequestDTO("TesteBusca", cpf,
-// "teste@busca.com");
-// ClienteResponseDTO cliente;
-// cliente = clienteRestAdapterController.insere(request);
+class ClienteServiceTest {
 
-// assertNotNull(cliente);
-// assertNotNull(cliente.getCpf());
-// assertEquals(cpf, cliente.getCpf());
+    private IClienteRepositoryAdapterGateway clienteRepositoryAdapterGateway;
+    private GestaoClienteUseCase gestaoClienteUseCase;
 
-// ClienteResponseDTO clienteBusca =
-// clienteRestAdapterController.buscaPorCpf(cpf);
-// assertNotNull(clienteBusca);
-// assertNotNull(clienteBusca.getCpf());
-// assertNotNull(clienteBusca.getCodigo());
-// assertEquals(cpf, clienteBusca.getCpf());
-// }
+    @BeforeEach
+    void setUp() {
+        clienteRepositoryAdapterGateway = mock(IClienteRepositoryAdapterGateway.class);
+        gestaoClienteUseCase = new GestaoClienteUseCase(clienteRepositoryAdapterGateway);
+    }
 
-// @Test
-// public void testeInsere() throws EntradaInvalidaException,
-// EntidadeNaoEncontradaException {
+    @Test
+    void insereClienteComCpfEDuplicado() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+      
+        when(clienteRepositoryAdapterGateway.buscaPorCpf(anyString())).thenReturn(Optional.of(new Cliente("codigo1", "Nome", "21618131885", "email@email.com.br")));
 
-// ClienteRequestDTO cliente = new ClienteRequestDTO("NomeTeste", "98765432109",
-// "teste@teste.com");
-// ClienteResponseDTO resultado = clienteRestAdapterController.insere(cliente);
-// assertNotNull(resultado);
+        Cliente clienteParaInserir = new Cliente("codigo2", "Novo Nome", "51757125868", "novoemail@email.com.br");
 
-// List<ClienteResponseDTO> clienteDTOS =
-// clienteRestAdapterController.buscaTodos();
-// assertFalse(CollectionUtils.isEmpty(clienteDTOS));
-// }
+        assertThrows(EntradaInvalidaException.class, () -> {
+            gestaoClienteUseCase.insere(clienteParaInserir);
+        });
+    }
 
-// @Test
-// public void testeRemove() throws EntidadeNaoEncontradaException,
-// EntradaInvalidaException {
+    @Test
+    void insereClienteComEmailDuplicado() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente com o mesmo e-mail já existe
+        when(clienteRepositoryAdapterGateway.buscaPorCpf(anyString())).thenReturn(Optional.empty());
+        when(clienteRepositoryAdapterGateway.buscaPorEmail(anyString())).thenReturn(Optional.of(new Cliente("codigo1", "Nome", "21618131885", "email@email.com")));
 
-// String codigoSaida;
-// AtomicReference<ClienteRequestDTO> cliente;
-// AtomicReference<ClienteResponseDTO> resultado;
+        Cliente clienteParaInserir = new Cliente("codigo2", "Novo Nome", "51757125868", "novoemail@email.com.br");
 
-// cliente = new AtomicReference<>();
-// resultado = new AtomicReference<>();
+        assertThrows(EntradaInvalidaException.class, () -> {
+            gestaoClienteUseCase.insere(clienteParaInserir);
+        });
+    }
 
-// cliente.set(new ClienteRequestDTO("NomeTeste", "45678912301",
-// "teste2@teste.com"));
-// resultado.set(clienteRestAdapterController.insere(cliente.get()));
-// codigoSaida = resultado.get().getCodigo();
+    @Test
+    void insereClienteComSucesso() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente não existe previamente
+        when(clienteRepositoryAdapterGateway.buscaPorCpf(anyString())).thenReturn(Optional.empty());
+        when(clienteRepositoryAdapterGateway.buscaPorEmail(anyString())).thenReturn(Optional.empty());
+        when(clienteRepositoryAdapterGateway.insere(any(Cliente.class))).thenReturn(new Cliente("codigo2", "Novo Nome", "51757125868", "novoemail@email.com.br"));
 
-// resultado.set(clienteRestAdapterController.busca(codigoSaida));
+        Cliente clienteParaInserir = new Cliente("codigo2", "Novo Nome", "51757125868", "novoemail@email.com.br");
 
-// assertNotNull(resultado);
+        Cliente clienteInserido = gestaoClienteUseCase.insere(clienteParaInserir);
+        assertNotNull(clienteInserido);
+    }
 
-// clienteRestAdapterController.remove(resultado.get().getCodigo());
-// assertThrows(
-// EntidadeNaoEncontradaException.class,
-// () -> clienteRestAdapterController.busca(resultado.get().getCodigo()));
-// assertThrows(
-// EntidadeNaoEncontradaException.class,
-// () -> clienteRestAdapterController.busca(codigoSaida));
+    @Test
+    void editaClienteComCodigoAusente() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o código do cliente está ausente
 
-// }
-// }
+        assertThrows(EntradaInvalidaException.class, () -> {
+            gestaoClienteUseCase.edita(new Cliente(null, "Novo Nome", "51757125868", "novoemail@email.com.br"));
+        });
+    }
+
+    @Test
+    void editaClienteInexistente() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente não existe
+        when(clienteRepositoryAdapterGateway.busca(anyString())).thenReturn(Optional.empty());
+
+        Cliente clienteParaEditar = new Cliente("1234", "Novo Nome", "51757125868", "novoemail@email.com.br");
+
+        assertThrows(EntidadeNaoEncontradaException.class, () -> {
+            gestaoClienteUseCase.edita(clienteParaEditar);
+        });
+    }
+
+    @Test
+    void editaClienteComSucesso() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente existe
+        when(clienteRepositoryAdapterGateway.busca(anyString())).thenReturn(Optional.of(new Cliente("123", "Novo Nome", "51757125868", "novoemail@email.com.br")));
+        when(clienteRepositoryAdapterGateway.atualiza(any(Cliente.class))).thenReturn(new Cliente("123", "Novo Nome2", "51757125868", "novoemail@email.com.br"));
+
+        Cliente clienteParaEditar = new Cliente("123", "Novo Nome", "51757125868", "novoemail@email.com.br");
+
+        Cliente clienteEditado = gestaoClienteUseCase.edita(clienteParaEditar);
+        assertEquals("Novo Nome2", clienteEditado.getNome());
+    }
+
+    @Test
+    void removeClienteComCodigoAusente() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o código do cliente está ausente
+        assertThrows(EntidadeNaoEncontradaException.class, () -> {
+            gestaoClienteUseCase.remove(null);
+        });
+    }
+
+    @Test
+    void removeClienteInexistente() throws EntidadeNaoEncontradaException, EntradaInvalidaException {
+        // Simular o cenário onde o cliente não existe
+        when(clienteRepositoryAdapterGateway.buscaPorCodigoEDataExclusaoNula(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(EntidadeNaoEncontradaException.class, () -> {
+            gestaoClienteUseCase.remove("codigo");
+        });
+    }
+
+    @Test
+    void removeClienteComSucesso() throws EntidadeNaoEncontradaException, EntradaInvalidaException {
+        // Simular o cenário onde o cliente existe
+        when(clienteRepositoryAdapterGateway.buscaPorCodigoEDataExclusaoNula(anyString())).thenReturn(Optional.of(new Cliente("123", "Novo Nome", "51757125868", "novoemail@email.com.br")));
+
+        assertDoesNotThrow(() -> {
+            gestaoClienteUseCase.remove("codigo");
+        });
+    }
+
+    @Test
+    void buscaClienteComCodigoAusente() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o código do cliente está ausente
+        assertThrows(EntradaInvalidaException.class, () -> {
+            gestaoClienteUseCase.busca(null);
+        });
+    }
+
+    @Test
+    void buscaClienteInexistente() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente não existe
+        when(clienteRepositoryAdapterGateway.busca(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(EntidadeNaoEncontradaException.class, () -> {
+            gestaoClienteUseCase.busca("codigo");
+        });
+    }
+
+    @Test
+    void buscaClienteComSucesso() throws EntradaInvalidaException, EntidadeNaoEncontradaException {
+        // Simular o cenário onde o cliente existe
+        when(clienteRepositoryAdapterGateway.busca(anyString())).thenReturn(Optional.of(new Cliente("123", "Novo Nome", "51757125868", "novoemail@email.com.br")));
+
+        Cliente cliente = gestaoClienteUseCase.busca("codigo");
+        assertNotNull(cliente);
+   
+
+}
+}
