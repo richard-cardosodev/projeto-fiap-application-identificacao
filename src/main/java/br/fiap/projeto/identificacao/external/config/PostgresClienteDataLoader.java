b47fb00ef85f15e816cc966e504429e571ff9d24
Richard Cardoso <richard.166@hotmail.com>
2024-01-25 23:49:21 -0300

    feat: mongodb 8

--- src/main/java/br/fiap/projeto/identificacao/external/config/PostgresClienteDataLoader.java
+++ src/main/java/br/fiap/projeto/identificacao/external/config/PostgresClienteDataLoader.java
@@ -4,6 +4,7 @@ import br.fiap.projeto.identificacao.entity.Cliente;
 import br.fiap.projeto.identificacao.usecase.port.IClienteRepositoryAdapterGateway;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +12,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-@Configuration @RequiredArgsConstructor
+@Configuration @RequiredArgsConstructor @Slf4j
 public class PostgresClienteDataLoader {
 
     private final IClienteRepositoryAdapterGateway clienteRepository;
@@ -19,6 +20,9 @@ public class PostgresClienteDataLoader {
     @PostConstruct
     @SneakyThrows
     public void init() {
+
+        log.info("Inserindo dados no repositorio da classe {}", clienteRepository.getClass().getName());
+
         List<Cliente> clientes = Collections.singletonList(
                 new Cliente(UUID.randomUUID().toString(), "Cliente1", "01234567890", "cliente1@test.com")
         );
